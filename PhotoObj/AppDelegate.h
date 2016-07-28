13adcf5b5025a29f9d6107cf2d8d0911d69ea2e3
pengxiuxiu <1479432699@qq.com>
2016-07-28 14:49:17 +0800

    Initial Commit

---
+++ PhotoObj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoObj
+//
+//  Created by pengxiuxiu on 16/7/28.
+//  Copyright © 2016年 pengxiuxiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoObj
+//
+//  Created by pengxiuxiu on 16/7/28.
+//  Copyright © 2016年 pengxiuxiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
